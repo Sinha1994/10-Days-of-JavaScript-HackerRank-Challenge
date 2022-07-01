e63661c50b2b32d7b13b05861c7284c6cc6751c7
Suman Sinha <sinha1194@gmail.com>
2022-07-01 23:15:14 +0530

    Day 4
    On day 4 we will learn to create classes and objects.

---
+++ Day 4/count_objects.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    let n = objects.length;
+    let count = 0;
+    for(let i=0;i<n;i++){
+        if(objects[i].x == objects[i].y){
+            count++;
+        }
+    }
+    return count;
+}
