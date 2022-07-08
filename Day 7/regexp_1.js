f4448acb630853d8bcd0da0412b5e563b82d066c
Suman Sinha <sinha1194@gmail.com>
2022-07-08 22:28:48 +0530

    day 7
    In the day 7 challenge, we will learn to use regular expressions in JavaScript

---
+++ Day 7/regexp_1.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function regexVar() {
+     
+    let re = /^([aeiou]).+\1$/;
+    return re;
+}
---
+++ Day 7/regexp_2.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    let re = new RegExp('^(Mr|Mrs|Ms|Dr|Er)(\\.)([A-Za-z])+$');
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
